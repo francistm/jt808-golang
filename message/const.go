07ae0779625089768508eda98969240c6f47da16
francis <francis.tm@gmail.com>
2023-10-31 09:44:32 +0000

    add const

--- message/const.go
+++ message/const.go
@@ -1,11 +1,26 @@
 package message
 
 const (
-	AckTypeSucceed byte = iota
-	AckTypeFailed
-	AckTypeIncorrect
-	AckTypeUnsupport
-	AckTypeAlarmConfirm
+	AckType_0001_Succeed byte = iota
+	AckType_0001_Failed
+	AckType_0001_Incorrect
+	AckType_0001_Unsupport
+)
+
+const (
+	AckType_8001_Succeed byte = iota
+	AckType_8001_Failed
+	AckType_8001_Incorrect
+	AckType_8001_Unsupport
+	AckType_8001_AlarmConfirm
+)
+
+const (
+	AckType_8100_Succeed byte = iota
+	AckType_8100_Vehicle_Registered
+	AckType_8100_Vehicle_NotFound
+	AckType_8100_Terminal_Registered
+	AckType_8100_Terminal_NotFound
 )
 
 const (
